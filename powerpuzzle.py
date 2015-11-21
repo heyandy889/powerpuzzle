6ebe716aba31aa431d7bfbe881f0b87abd00ae9f
heyandy889 <heyandy889@users.noreply.github.com>
2015-11-20 22:20:47 -0500

    it's working now!

--- powerpuzzle.py
+++ powerpuzzle.py
@@ -6,20 +6,33 @@ import random
 #whose sums are equal.
 #also sums of powers up to k are equal.
 def solve(k):
-	integers = range(int(math.pow(2,k+1)))
-	halfsies = len(integers)/2
 
-	set1 = []
-	set2 = []
-	while len(set1) < halfsies:
-		x = random.choice(integers)
-		set1.append(x)
-		integers.remove(x)
+	while True:
+		integers = range(int(math.pow(2,k+1)))
+		halfsies = len(integers)/2
 
-	while len(set2) < halfsies:
-		x = random.choice(integers)
-		set2.append(x)
-		integers.remove(x)
+		set1 = []
+		set2 = []
+		while len(set1) < halfsies:
+			x = random.choice(integers)
+			set1.append(x)
+			integers.remove(x)
 
-	return [set1,set2]
+		while len(set2) < halfsies:
+			x = random.choice(integers)
+			set2.append(x)
+			integers.remove(x)
+
+		exp = 1
+		things_are_ok = True
+		while things_are_ok and exp < k+1:
+			a = [i**exp for i in set1]
+			b = [i**exp for i in set2]
+			if sum(a) != sum(b):
+				things_are_ok = False
+			exp += 1
+
+		if things_are_ok:
+			return [set1, set2]
+
 
--- test.py
+++ test.py
@@ -7,10 +7,15 @@ def check(expected, k):
 	actual[0].sort()
 	expected[1].sort()
 	actual[1].sort()
-	assert expected == actual, '\n' + str(expected) + '\n' + str(actual)
+	if expected != actual:
+		if expected[0] != actual[1]:
+			print('no\n' + str(expected) + '\n' + str(actual))
+			print('')
 
 check([[1,2],[0,3]], 1)
 
 check([[1, 2, 4, 7],[0, 3, 5, 6]], 2)
 
+
+check([[1, 4, 2, 11, 7, 8, 13, 14], [12, 10, 6, 15, 0, 9, 3, 5]], 3)
 
