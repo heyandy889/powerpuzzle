e7f9bdca919f7a33ea2406481ac1fbbde0967708
heyandy889 <heyandy889@users.noreply.github.com>
2015-11-21 00:13:10 -0500

    solution for all powers up to 4 now

--- powerpuzzle.py
+++ powerpuzzle.py
@@ -1,25 +1,18 @@
 
 import math
 import random
+from time import sleep
 
 #For all the numbers 0-n, divide into 2 sets
 #whose sums are equal.
 #also sums of powers up to k are equal.
-def solve(k):
-
-	from cache import cache
-
-	#f = open('cache.py', 'w+')
-	#f.write('cache = [')
-	#N = 0
-
+def solve(k, start=0):
 
 	while True:
-		integers = range(int(math.pow(2,k+1)))
-		halfsies = len(integers)/2
-
-		set1 = []
-		set2 = []
+		integers = range(start, int(math.pow(2,k+1)))
+		halfsies = int(math.pow(2,k+1)) / 2
+		set1 = [1, 4, 2, 11, 7, 8, 13, 14]
+		set2 = [12, 10, 6, 15, 0, 9, 3, 5]
 		while len(set1) < halfsies:
 			x = random.choice(integers)
 			set1.append(x)
@@ -29,26 +22,15 @@ def solve(k):
 			x = random.choice(integers)
 			set2.append(x)
 			integers.remove(x)
-
-		if [set1, set2] in cache:
-			print('whoopsie daisy')
-			pass
-		exp = 4
+		#print[set1,set2]
+		exp = k
 		things_are_ok = True
-
-		while things_are_ok and exp < k+1:
+		while things_are_ok and exp > 0:
 			a = [i**exp for i in set1]
 			b = [i**exp for i in set2]
 			if sum(a) != sum(b):
 				things_are_ok = False
-				#if N < 1000000:
-				#	f.write('[' + str(set1) + ',' + str(set2) + '],\n')
-				#	N += 1
-				#if(exp > 3):
-				#	print exp
-				#	print set1
-				#	print set2
-			exp += 1
+			exp -= 1
 
 		if things_are_ok:
 			return [set1, set2]
--- test.py
+++ test.py
@@ -1,23 +1,24 @@
 from powerpuzzle import solve
 
-def check(expected, k):
-	actual = solve(k)
+def check(expected, k, start=0):
+	actual = solve(k=k, start=start)
 
 	expected[0].sort()
 	actual[0].sort()
 	expected[1].sort()
 	actual[1].sort()
-	if expected != actual:
-		if expected[0] != actual[1]:
-			print('no\n' + str(expected) + '\n' + str(actual))
-			print('')
+	if expected != actual and expected[0] != actual[1]:
+		print('no\nexpected: ' + str(expected) + '\nactual: ' + str(actual))
+		print('')
+	else:
+		print('yay! k=' + str(k) + ', ' + str(actual))
 
-check([[1,2],[0,3]], 1)
+check([[1,2],[0,3]], k=1, start=0)
 
-check([[1, 2, 4, 7],[0, 3, 5, 6]], 2)
+check([[1, 2, 4, 7],[0, 3, 5, 6]], k=2, start=4)
 
 
-check([[1, 4, 2, 11, 7, 8, 13, 14], [12, 10, 6, 15, 0, 9, 3, 5]], 3)
+check([[1, 4, 2, 11, 7, 8, 13, 14], [12, 10, 6, 15, 0, 9, 3, 5]], k=3, start=16)
 
-print(solve(4))
+print(solve(k=4, start=16))
 
